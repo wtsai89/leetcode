4051e968656a8e20057add25de95c3de091892c3
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2022-11-22 20:26:44 -0800

    11/22/22

--- Implement strStr.py
+++ Implement strStr.py
@@ -1,16 +1,14 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)-len(needle)+1):
-            if haystack[i] == needle[0]:
-                nindex = 1
-                flag = 1
-                for j in range(i+1,i+len(needle)):
-                    if haystack[j] != needle[nindex]:
-                        flag = 0
-                        break
-                    nindex += 1
-                if flag:
-                    return i
+        m, n = len(haystack), len(needle)
+        for i in range(m-n+1):
+            nind = 0
+            hind = i
+            while nind < n and haystack[hind] == needle[nind]:
+                nind += 1
+                hind += 1
+            if nind == n:
+                return i
                 
         return -1
 
