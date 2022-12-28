b9d11a4832f096757c1683edf80aeb2c7af1d0ac
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2022-12-27 16:27:36 -0800

    12/27/22

---
+++ Roman To Integer.py
@@ -0,0 +1,34 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900
+        }
+        
+        ans = 0
+        i = 0
+        while i < len(s)-1:
+            if s[i:i+2] in d:
+                ans += d[s[i:i+2]]
+                i += 2
+            else:
+                ans += d[s[i]]
+                i += 1
+        if i == len(s)-1:
+            ans += d[s[-1]]
+                
+        return ans
+
+s = Solution()
+print(s.romanToInt("III"))
