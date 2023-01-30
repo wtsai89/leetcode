af17a6c2c56664dec7c36ebb1171d9e1c388680e
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2023-01-30 10:31:17 -0800

    1/30/23

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -4,18 +4,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = len(nums1) - 1
-        m -= 1
-        n -= 1
-        while n >= 0:
-            if m < 0 or nums2[n] > nums1[m]:
-                nums1[i] = nums2[n]
-                n -= 1
-            else:
-                nums1[i] = nums1[m]
-                m -= 1
-            i -= 1
+        idx, p1, p2 = len(nums1)-1, m-1, n-1
 
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[idx] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[idx] = nums2[p2]
+                p2 -= 1
+            idx -= 1
+        
+        while p2 >= 0:
+            nums1[idx] = nums2[p2]
+            p2 -= 1
+            idx -= 1   
 s = Solution()
 nums1 = [1,2,3,0,0,0]
 nums2 = [2,5,6]
