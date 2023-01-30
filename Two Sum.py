fcbf33c8eabcec7971c5e4f50353a3b110d3b822
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2023-01-30 10:18:06 -0800

    1/30/23

--- Two Sum.py
+++ Two Sum.py
@@ -2,11 +2,14 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
+
         for i in range(len(nums)):
-            if nums[i] in d:
-                return [i, d[nums[i]]]
-            else:
-                d[target-nums[i]] = i
+            val = nums[i]
+            if val in d:
+                return [i, d[val]]
+            d[target-val] = i
+
+        return []
 
 s = Solution()
 a = [2,7,11,15]
