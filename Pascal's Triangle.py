fdb417a5b561ef5f40f3d2d6474619373ae81732
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2023-01-31 15:21:15 -0800

    1/31/23

--- Pascal's Triangle.py
+++ Pascal's Triangle.py
@@ -1,16 +1,15 @@
 from typing import List
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        s = [[1]]
-        if numRows == 1:
-            return s
+        ans = [[1]]
         for i in range(numRows-1):
+            prev = ans[i]
             row = [1]
-            for j in range(len(s[i])-1):
-                row.append(s[i][j]+s[i][j+1])
+            for j in range(len(prev)-1):
+                row.append(prev[j]+prev[j+1])
             row.append(1)
-            s.append(row)
-        return s
+            ans.append(row)
+        return ans
 
 s = Solution()
 print(s.generate(5))
