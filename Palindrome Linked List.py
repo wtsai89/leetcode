5f220ed06139a010e337f7ee8b04662fe5d8014d
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2023-02-14 16:07:01 -0800

    2/14/23

--- Palindrome Linked List.py
+++ Palindrome Linked List.py
@@ -1,36 +1,40 @@
 from ListNode import *
-
+from typing import Optional
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        #find mid
-        p1, p2 = head, head
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head.next:
+            return True
         
-        while p2.next and p2.next.next:
+        def reverse(head):
+            curr = head
+            prev = None
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            return prev
+    
+        p1,p2 = head,head
+        while p1:
             p1 = p1.next
-            p2 = p2.next.next
-        
-        #reverse second half
-        curr = p1.next
-        prev = None
-        
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        
-        #compare 1st and 2nd half
-        while prev:
-            if head.val != prev.val:
+            p2 = p2.next
+            if p1:
+                p1 = p1.next
+        p1 = head
+        p2 = reverse(p2)
+
+        while p2:
+            if p1.val != p2.val:
                 return False
-            head = head.next
-            prev = prev.next
-        
+            p1 = p1.next
+            p2 = p2.next
+
         return True
 
 s = Solution()
